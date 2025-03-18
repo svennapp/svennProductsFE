cc5c2c9f90ace1eb52a878591b376dab639dd33c
Ole <olejorgen.naess@gmail.com>
2025-03-18 17:28:45 +0100

    navigation clean up

--- lib/menu-list.ts
+++ lib/menu-list.ts
@@ -2,22 +2,14 @@ import {
   Package,
   Workflow,
   Settings2,
-  LogOut,
   LucideIcon
 } from "lucide-react";
-
-type Submenu = {
-  href: string;
-  label: string;
-  active?: boolean;
-};
 
 type Menu = {
   href: string;
   label: string;
   active?: boolean;
   icon: LucideIcon;
-  submenus?: Submenu[];
 };
 
 type Group = {
@@ -34,50 +26,19 @@ export function getMenuList(pathname: string): Group[] {
           href: "/products",
           label: "Products",
           icon: Package,
-          active: pathname.startsWith("/products"),
-          submenus: [
-            {
-              href: "/products",
-              label: "View Products",
-              active: pathname === "/products"
-            }
-          ]
+          active: pathname.startsWith("/products")
         },
         {
           href: "/scripts",
           label: "Spider Scripts",
           icon: Workflow,
-          active: pathname.startsWith("/scripts"),
-          submenus: [
-            {
-              href: "/scripts",
-              label: "View Scripts",
-              active: pathname === "/scripts"
-            },
-            {
-              href: "/scripts/history",
-              label: "Run History",
-              active: pathname === "/scripts/history"
-            }
-          ]
+          active: pathname.startsWith("/scripts")
         },
         {
           href: "/settings",
           label: "Settings",
           icon: Settings2,
-          active: pathname.startsWith("/settings"),
-          submenus: [
-            {
-              href: "/settings/general",
-              label: "General",
-              active: pathname === "/settings/general"
-            },
-            {
-              href: "/settings/account",
-              label: "Account",
-              active: pathname === "/settings/account"
-            }
-          ]
+          active: pathname.startsWith("/settings")
         }
       ]
     }
